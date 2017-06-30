d6041a83bcd0add4f7a4fa1d8ff7895319ca1dfd
YAP! <GMADDA200@caledonian.ac.uk>
2017-06-30 21:13:58 +0100

    motion + jump base working

---
+++ Assets/Code/GroundChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour {
+
+    public bool m_isOnGround = true;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ground")
+            m_isOnGround = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Ground")
+            m_isOnGround = false;
+    }
+}
---
+++ Assets/Code/TabletInputMover.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TabletInputMover : MonoBehaviour {
+
+    #region member variables
+
+    public float m_speed;
+    public float m_jumpSpeed;
+    [SerializeField]
+    public Text m_text;
+
+    private Rigidbody m_rb;
+    private GroundChecker m_groundChecker;
+    private bool m_canJump = true;
+
+    #endregion
+
+    void Start ()
+    {
+        m_rb = GetComponent<Rigidbody>();
+        m_groundChecker = GetComponentInChildren<GroundChecker>();
+	}
+	
+	void Update ()
+    {
+        m_text.text = Input.acceleration.x.ToString() + ", " + Input.acceleration.y.ToString() + ", " + Input.acceleration.z.ToString();
+
+        if (Mathf.Abs(Input.acceleration.x) > 0.1)
+            m_rb.AddForce(Vector3.right * -m_speed * Mathf.Sign(Input.acceleration.x), ForceMode.Force);
+
+        if (Mathf.Abs(Input.acceleration.y) > 0.1)
+            m_rb.AddForce(Vector3.forward * -m_speed * Mathf.Sign(Input.acceleration.y), ForceMode.Force);
+
+        if (Input.acceleration.z > 0.6 && m_canJump)
+        {
+            m_canJump = false;
+            m_rb.AddForce(Vector3.up * m_jumpSpeed, ForceMode.Impulse);
+        }
+
+        if (m_groundChecker.m_isOnGround)
+            m_canJump = true;
+        else
+            m_rb.AddForce(Vector3.up * -10, ForceMode.VelocityChange);
+    }
+}
