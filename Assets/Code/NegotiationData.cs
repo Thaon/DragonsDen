62b384e8959e10509ef98505349101bdd3cba137
Agleplop <arduncan111@gmail.com>
2017-07-01 12:09:18 +0100

    Fungus and Negotiation

---
+++ Assets/Code/NegotiationData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NegotiationData : MonoBehaviour 
+{
+
+	public Text moneyWantedText;
+	public Slider moneySlider;
+	public Text percentageText;
+	public Slider percentageSlider;
+
+	int currentMoney = 10;
+	int moneyWanted;
+	int percentageAvailable;
+
+	// Use this for initialization
+	void Start () 
+	{
+		moneyWanted = (int)Mathf.Round (currentMoney + currentMoney * moneySlider.value * 1.5f);
+		moneyWantedText.text = moneyWanted.ToString ();
+		percentageAvailable = (int)Mathf.Round ( percentageSlider.value * 51);
+		percentageText.text = percentageAvailable.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		moneyWanted = (int)Mathf.Round (currentMoney + currentMoney * moneySlider.value * 1.5f);
+		moneyWantedText.text = moneyWanted.ToString ();
+		percentageAvailable = (int)Mathf.Round ( percentageSlider.value * 51);
+		percentageText.text = percentageAvailable.ToString ();
+	}
+
+}
