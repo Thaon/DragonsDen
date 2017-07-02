5dcd8349e883f5e7cf681b998b434f43d48928bf
YAP! <GMADDA200@caledonian.ac.uk>
2017-07-02 15:29:28 +0100

    added voices

---
+++ Assets/Code/Voices.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class Voices : MonoBehaviour {
+
+    private StudioEventEmitter m_emitter;
+
+    public bool m_canPlay = false;
+
+	void Start ()
+    {
+        m_emitter = GetComponent<StudioEventEmitter>();
+        StartCoroutine(SayVoice());
+	}
+	
+	void Update () {
+		
+	}
+
+    public void VoiceOn()
+    {
+        m_canPlay = true;
+    }
+
+    public void VoiceOff()
+    {
+        m_canPlay = false;
+    }
+
+    IEnumerator SayVoice()
+    {
+        yield return new WaitForSeconds(0.1f);
+        if (m_canPlay)
+            m_emitter.Play();
+        StartCoroutine(SayVoice());
+    }
+}
